58dc2d75d8ee70c98acec72b4c19c96f62593fa7
yhliu <yhliu@gizwits.com>
2017-03-01 00:19:47 +0800

    commit

--- src/com/jfinal/ext/handler/JsonParameterHandler.java
+++ src/com/jfinal/ext/handler/JsonParameterHandler.java
@@ -3,6 +3,7 @@ package com.jfinal.ext.handler;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.jfinal.handler.Handler;
 import com.jfinal.kit.HttpKit;
+import com.jfinal.kit.JsonKit;
 import com.jfinal.kit.StrKit;
 
 /**
@@ -26,18 +28,29 @@ public class JsonParameterHandler extends Handler {
 
 class JsonParameterRequestWrapper extends HttpServletRequestWrapper {
 	private Map<String, String[]> params;
+	
+	@SuppressWarnings("unchecked")
 	public JsonParameterRequestWrapper(HttpServletRequest request) {
 		super(request);
 		Map<String, String[]> clone = new HashMap<String, String[]>();
 		String contentType = request.getContentType();
-		if(!StrKit.isBlank(contentType) && "application/json;".equalsIgnoreCase(contentType)){
+		if(!StrKit.isBlank(contentType) && "application/json;charset=utf-8".equalsIgnoreCase(contentType)){
 			String body = HttpKit.readData(request);
 			if(!StrKit.isBlank(body)){
-				System.out.println(body);
+				Map<String,Object> map = JsonKit.parse(body, Map.class);
+				for(Entry<String, Object> entry : map.entrySet()){
+					if(entry.getValue() != null){
+						if(entry.getValue().getClass().isArray()){
+							
+						} else{
+							clone.put(entry.getKey(), new String[]{entry.getValue().toString()});
+						}
+					}
+				}
 			}
 		}
 		params = clone;
-		Collections.unmodifiableMap(clone);
+		Collections.unmodifiableMap(params);
 	}
 	
     public Map<String, String[]> getParameterMap() {  
