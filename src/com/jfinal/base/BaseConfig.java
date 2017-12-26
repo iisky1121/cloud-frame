2fe5bf4cbf231c6f323cb0fbebf20f4fe237a288
yhliu <yhliu@gizwits.com>
2017-12-26 14:26:10 +0800

    V1.9.4.2 sys_error_code完善

--- src/com/jfinal/base/BaseConfig.java
+++ src/com/jfinal/base/BaseConfig.java
@@ -12,19 +12,19 @@ public class BaseConfig {
 	public final static String failure_msg = "操作失败";
 	
 	public static ReturnResult notLogin(){
-		return ReturnResult.failure("请先登录再操作", "sys_authorization_exception");
+		return ReturnResult.failure("请先登录再操作", "sys_no_login", null);
 	}
 	
 	public static ReturnResult notPermission(String actionKey){
-		return ReturnResult.failure(String.format("未被授权访问该资源[%s]", actionKey), "sys_no_permission");
+		return ReturnResult.failure(String.format("未被授权访问该资源[%s]", actionKey), "sys_no_permission", null);
 	}
 	
 	public static ReturnResult attrValueError(String attr){
-		return ReturnResult.failure(String.format("属性[%s]值有误", attr), "sys_validate_attr_value_error");
+		return ReturnResult.failure(String.format("属性[%s]值有误", attr), "sys_validate_attr_value_error", null);
 	}
 	
 	public static ReturnResult attrValueEmpty(String attr){
-		return ReturnResult.failure(String.format("属性[%s]不允许为空", attr), "sys_validate_attr_value_empty");
+		return ReturnResult.failure(String.format("属性[%s]不允许为空", attr), "sys_validate_attr_value_empty", null);
 	}
 	
 	public static ReturnResult attrEitherOr(String attr, String[] eitherOr){
@@ -33,7 +33,7 @@ public class BaseConfig {
 		for(String eOr : eitherOr){
 			set.add(eOr);
 		}
-		return ReturnResult.failure(String.format("属性%s至少一个不为空", Arrays.toString(set.toArray())), "sys_validate_attr_eitherOr");
+		return ReturnResult.failure(String.format("属性%s至少一个不为空", Arrays.toString(set.toArray())), "sys_validate_attr_eitherOr", null);
 	}
 	
 	public static ReturnResult attrAssociation(String attr, String[] association){
@@ -41,18 +41,18 @@ public class BaseConfig {
 		for(String ass : association){
 			set.add(ass);
 		}
-		return ReturnResult.failure(String.format("属性[%s]存在,属性%s都不能为空", attr, Arrays.toString(set.toArray())), "sys_validate_attr_association");
+		return ReturnResult.failure(String.format("属性[%s]存在,属性%s都不能为空", attr, Arrays.toString(set.toArray())), "sys_validate_attr_association", null);
 	}
 	
 	public static ReturnResult dataNotExist(){
-		return ReturnResult.failure("数据不存在", "sys_data_not_exist");
+		return ReturnResult.failure("数据不存在", "sys_data_not_exist", null);
 	}
 	
 	public static ReturnResult dataError(){
-		return ReturnResult.failure("数据错误", "sys_data_error");
+		return ReturnResult.failure("数据错误", "sys_data_error", null);
 	}
 	
 	public static ReturnResult sysError(){
-		return ReturnResult.failure("系统错误", "sys_error");
+		return ReturnResult.failure("系统错误", "sys_error", null);
 	}
 }
